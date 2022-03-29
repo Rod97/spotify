f2f001bd76605304e04dc7c2be047bc0aa2e239e
Rodrigo <rflores456123@gmail.com>
2022-03-29 12:56:53 -0400

    initial commit

---
+++ ui/windows/ui/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ui::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ui::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
