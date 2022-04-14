6448699f0c33abb93d110e194f64005191e3f3b3
Rodrigo <rflores456123@gmail.com>
2022-04-14 18:29:12 -0400

    spotify redirect working

--- ui/src/components/login.js
+++ ui/src/components/login.js
@@ -2,23 +2,35 @@ import React, { useState } from "react";
 import { Buttons } from "react-native/Libraries/Alert/Alert";
 import { View, Text, Button } from 'react-native';
 import { WebView } from 'react-native-webview'
-import randomstring from 'randomstring';
 import base64url from 'base64url'
 
 
 const Login = () => {
-    const [htmlString, setHtmlString] = useState('')
+    const [uri, setUri] = useState('');
+    const [authCode, setAuthCode] = useState('');
 
-    const generateChallenge = () => {
-        console.log(randomstring.generate())
-        // const code_verifier = randomstring.generate(128);
-        // return code_verifier;
-        // const base64Digest = crypto.createHash('sha256')
-        //     .update(code_verifier)
-        //     .digest('base64');
-        // const code_challenge = base64url.fromBase64(base64Digest);
+    // const generateChallenge = () => {
 
-        // return code_challenge;
+    //     const code_verifier = randomString(16);
+    //     const base64Digest = crypto.
+    //     createHash('sha256')
+    //         .update(code_verifier)
+    //         .digest('base64');
+    //     const code_challenge = base64url.fromBase64(base64Digest);
+
+    //     return code_challenge;
+    // }
+
+    const randomString = (length) => {
+        let randString = '';
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv123456789-._~'
+        const charactersLength = chars.length;
+
+        for (let i = 0; i < length; i++) {
+            randString += chars.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return randString
     }
 
     const sendRequest = () => {
@@ -27,17 +39,27 @@ const Login = () => {
         const response_type = 'code';
         const redirect_uri = 'http://localhost'
         const scope = 'user-read-email';
-        const code_challenge_method = 'S256';
-        const code_challenge = generateChallenge();
-        const queryString = `${client_id}&${response_type}&${redirect_uri}&${scope}&${code_challenge_method}&${code_challenge}`;
+        // const code_challenge_method = 'S256';
+        // const code_challenge = generateChallenge();
+        const queryString = `client_id=${client_id}&response_type=${response_type}&redirect_uri=${redirect_uri}&scope=${scope}&show_dialog=true`;
+        // &${code_challenge_method}&${code_challenge}`;
 
-        fetch(`${url}${queryString}`).then((res) => {
-            console.log({ res })
-            setHtmlString('<h1>I have returned</h1>');
-        })
-            .catch((e) => {
-                console.log(e)
-            });
+        setUri(`${url}${queryString}`)
+        // fetch(`${url}${queryString}`).then((res) => {
+        //     console.log({ res })
+        //     setHtmlString('<h1>I have returned</h1>');
+        // })
+        //     .catch((e) => {
+        //         console.log(e)
+        //     });
+    }
+
+    const handleAuthResponse = (res) => {
+        if(res.url.includes('http://localhost/?code=')){
+            setUri(`<h1>${res.url.substring(22)}</h1>`);
+            setAuthCode(res.url.substring(22));
+        }
+        console.log(res)
     }
     return (
         <View
@@ -48,10 +70,11 @@ const Login = () => {
                 color='black'
             />
             {
-                htmlString ?
+                uri ?
                     <WebView
-                        source={{ html: htmlString }}
-                        style={{ flex: 1, textColor: 'black' }} /> :
+                        source={{ uri: uri }}
+                        style={{ flex: 1, textColor: 'black' }}
+                        onNavigationStateChange={handleAuthResponse} /> :
                     null
             }
         </View>
